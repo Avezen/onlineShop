7f6573c446e0af32cb4deef284798908c9ac2498
avezen <todiexxx@gmail.com>
2018-07-10 22:33:04 +0200

    css almost finished

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -32,7 +32,7 @@ class ProductController extends FOSRestController
         $pagination = $paginator->paginate(
             $products,
             $request->query->getInt('page', 1),
-            $request->query->getInt('limit',5)
+            $request->query->getInt('limit',6)
         );
 
         $pagination->setCustomParameters(array(
